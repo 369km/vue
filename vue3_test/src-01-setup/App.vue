d3b8588cb58318f27621af35d443705214ec50e1
yfd <yangfudong@189.cn>
2021-08-02 17:01:47 +0800

    setup ref

---
+++ vue3_test/src-01-setup/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>setup</h1>
+  <h3>{{outline}}</h3>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  setup(){
+    let outline='setup 中包含了数据，方法，钩子'
+    return{
+      outline,
+    }
+  }
+}
+</script>
