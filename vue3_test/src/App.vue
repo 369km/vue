736c8a9a4c7088b047cd2ceb33c12360b050832f
yfd <yangfudong@189.cn>
2021-08-03 10:26:47 +0800

    reactive

--- vue3_test/src/App.vue
+++ vue3_test/src/App.vue
@@ -1,34 +1,35 @@
 <template>
-  <h1>ref</h1>
-  <h3>{{ str }}</h3>
-  <button @click="changeStr">ref str</button>
-  <h3>{{ obj.b }}</h3>
-  <button @click="changeObj">ref obj</button>
+  <h1>reactive</h1>
+  <h3>{{ data.name }}</h3>
+  <p>{{ data.age }}</p>
+  <p>{{ data.hobby }}</p>
+  <p>{{ data.children }}</p>
+  <button @click="changeHobby">改变爱好</button>
 </template>
 
 <script>
-import {ref} from 'vue'
+import {reactive} from 'vue'
 
 export default {
   name: 'App',
   setup() {
-    let str = ref('let outline = ref(a) use {{a}}')
-    let obj=ref({
-      b:'let obj=ref({b=xxx}) use {{obj.b}}'
+    let data = reactive({
+      name: '于谦',
+      age: 59,
+      hobby: ['抽烟', '喝酒', '烫头'],
+      children: {
+        name: '于思洋',
+        age: 6
+      }
     })
 
-    function changeStr() {
-      str.value = 'change a.value=xxx'
+    function changeHobby() {
+      data.hobby[0] = '相声'
     }
 
-    function changeObj(){
-      obj.value.b='change obj.value.b=xxx'
-    }
     return {
-      str,
-      obj,
-      changeStr,
-      changeObj
+      data,
+      changeHobby
     }
   }
 }
